7e2038c42fc48921e548f31bf0cf2aaf8a0220a3
Ryula <hobby519@naver.com>
2023-10-14 02:33:43 +0900

    Revert "Revert "init""
    This reverts commit 4a8e8f18a275e5deaf0f4ee20006d56c3b2734c5.

---
+++ src/test/java/inha/how/HowApplicationTests.java
@@ -0,0 +1,13 @@
+package inha.how;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
