b088b1db5712d5cf021357392be36d5bf5f089f6
Ryula <hobby519@naver.com>
2023-10-31 01:48:48 +0900

    [fix] swagger cors 해결

--- src/main/java/inha/how/HowApplication.java
+++ src/main/java/inha/how/HowApplication.java
@@ -1,8 +1,16 @@
 package inha.how;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@OpenAPIDefinition(
+		servers = {
+				@Server(url="http://52.78.0.53", description = "Default Server url")
+		}
+)
 
 //시큐리티 기능 끄기
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
